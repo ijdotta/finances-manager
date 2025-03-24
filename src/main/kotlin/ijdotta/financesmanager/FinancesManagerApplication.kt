a4a40980c2b59b0a2a2aa2061b9bed528f468c69
ijdotta <ij.dotta@gmail.com>
2025-03-24 14:19:56 -0300

    initial commit

---
+++ src/main/kotlin/ijdotta/financesmanager/FinancesManagerApplication.kt
@@ -0,0 +1,11 @@
+package ijdotta.financesmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinancesManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<FinancesManagerApplication>(*args)
+}
---
+++ src/test/kotlin/ijdotta/financesmanager/FinancesManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ijdotta.financesmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinancesManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
